147eb9e49707d54675a819dcec688674a104d2c6
John Wellesz <John.Wellesz@teaser.fr>
2014-11-22 16:57:10 +0100

    Switch to GPLv3 license

--- fileCreationProfiler.php
+++ fileCreationProfiler.php
@@ -1,9 +1,23 @@
 #!/usr/bin/php
 <?php
-/**
- * File creation/deletion profiling script v1.02
- * Author: John Wellesz
- * License: public domain
+/*
+    File creation/deletion profiling script v1.03
+
+    Copyright © 2014 by John Wellesz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
  */
 
 # Usage:
@@ -32,7 +46,7 @@ $TEST_FILE_BASE_PATH = __FILE__ . "_test-";
 ##### END OF CONFIGURATION #######
 
 const MAJOR = 1;
-const MINOR = 2;
+const MINOR = 3;
 
 
 $WHEEL = '-\\|/';
